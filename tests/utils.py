01f5bd2b64ccc998cb593a71b4c2bcb1204ba416
notjoao1 <joao.p.dourado1@gmail.com>
2024-09-12 17:36:36 +0100

    fix(test): util function to check whether two folders are equal now checks both source = replica and replica = source

--- tests/utils.py
+++ tests/utils.py
@@ -18,17 +18,27 @@ def get_random_temp_folder_path():
     if not os.path.exists(folder_path):
       return folder_path
 
-def assert_equal_folders(source_folder_path, replica_folder_path):
+def equal_folders(source_folder_path, replica_folder_path) -> bool:
   """
-    Walks replica folder and, for every file, ensures the same exists in the source
-    folder and the hash of the source counterpart is the same. 
+    Walks both folders and, for every file, ensures the same exists in the counterpart
+    and the hash is the same. If that's the case, returns true, otherwise, returns false
   """
+  for dirpath, _, fnames in os.walk(source_folder_path):
+    source_rel_path = os.path.relpath(dirpath, source_folder_path)
+    equivalent_replica_path = os.path.join(replica_folder_path, source_rel_path)
+    for fname in fnames:
+      replica_fpath = os.path.join(equivalent_replica_path, fname)
+      source_fpath = os.path.join(dirpath, fname)
+      if not os.path.exists(replica_fpath) or hash_file(source_fpath) != hash_file(replica_fpath):
+        return False
+
   for dirpath, _, fnames in os.walk(replica_folder_path):
     replica_rel_path = os.path.relpath(dirpath, replica_folder_path)
     equivalent_source_path = os.path.join(source_folder_path, replica_rel_path)
     for fname in fnames:
       source_fpath = os.path.join(equivalent_source_path, fname)
       replica_fpath = os.path.join(dirpath, fname)
-      assert os.path.exists(source_fpath)
-      assert hash_file(source_fpath) == hash_file(replica_fpath)
+      if not os.path.exists(source_fpath) or hash_file(source_fpath) != hash_file(replica_fpath):
+        return False
 
+  return True
