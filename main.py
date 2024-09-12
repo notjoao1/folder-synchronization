59ffc4d143ee89a97e48e32089b6dd5d6accf4a2
notjoao1 <joao.p.dourado1@gmail.com>
2024-09-12 14:05:51 +0100

    chore: proper logging instead of print statements + type hints

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="One-way synchronization between two folders.")
   parser.add_argument('-s', '--source', type=str, required=True, help="Source folder that will be replicated in the 'replica' folder.")
   parser.add_argument('-r', '--replica', type=str, required=True, help="Replica folder that will be synchronized with the 'source' folder.")
-  parser.add_argument('-i', '--interval', type=int, required=True, help="Interval for periodically synchronizing the 'replica' folder with the 'source' folder.")
+  parser.add_argument('-i', '--interval', type=int, required=True, help="Interval (in seconds) for periodically synchronizing the 'replica' folder with the 'source' folder.")
   parser.add_argument('-l', '--log-file', type=str, required=True, help="Log file path.")
 
   args = parser.parse_args() 
--- src/folder_synchronizer.py
+++ src/folder_synchronizer.py
@@ -13,36 +13,37 @@ class FolderSynchronizer:
     self._source_file_index = dict()
     self._setup_logger(log_file_path)
 
-  def periodic_sync(self):
+  def periodic_sync(self) -> None:
     """
       Periodically synchronizes source and replica folder in a periodic manner,
       according to the given interval. 
     """
+    self._logger.info(f"Starting synchronization between source folder '{self.source_path}' and replica folder '{self.replica_path} every {self.interval} seconds.")
     while True:
       init = time.time() 
       self.sync_folders()
-      print(f"Files synchronized successfully in {time.time() - init:.3f}s")
+      self._logger.info(f"Synchronized replica and source in {time.time() - init:.3f} seconds...")
       time.sleep(self.interval)
 
-  def sync_folders(self):
+  def sync_folders(self) -> None:
     """
       Performs one-way synchronization between source folder and replica folder. 
     """
     self._sync_source_to_replica()
     self._sync_replica_to_source()
 
-  def _sync_source_to_replica(self):
+  def _sync_source_to_replica(self) -> None:
     """
       Walks source directory and copies files from source to replica if they don't exist, or their
       hash has been modified, or their metadata changed.
     """
-    print("Replicating source inside of replica...")
     for dirpath, _, fnames in os.walk(self.source_path):
       # relative path from source to the current "dirpath"
       src_rel_path = os.path.relpath(dirpath, self.source_path)
       replica_folder_path = os.path.join(self.replica_path, src_rel_path)
 
       if not os.path.exists(replica_folder_path):
+        self._logger.info(f"Folder '{replica_folder_path}' doesn't exist in replica, creating it...")
         os.makedirs(replica_folder_path, exist_ok=True)
 
       for fname in fnames:
@@ -50,54 +51,51 @@ class FolderSynchronizer:
         replica_fpath = os.path.join(replica_folder_path, fname)
 
         if not os.path.exists(replica_fpath):
-          print(f"Copying {source_fpath} to {replica_fpath}, since it didn't exist")
+          self._logger.info(f"File '{source_fpath}' doesn't exist in replica, copying it over to '{replica_fpath}'...")
           copy_file(source_fpath, replica_fpath)
           continue
 
         if not equal_metadata(source_fpath, replica_fpath):
-          print(f"Detected metadata change in {source_fpath}.")
+          self._logger.info(f"Detected metadata change in '{source_fpath}', copying metadata to '{replica_fpath}'")
           copy_metadata(source_fpath, replica_fpath)
           # try to compare cached hash with the current hash, and if there's no cached hash
           # just calculate both replica and source hash and compare them to know if contents changed 
           if source_fpath in self._source_file_index:
-            print(f"Already had cached the {source_fpath} hash, only gonna calculate itself again and check for changes")
+            self._logger.debug(f"Hash cache hit for key '{source_fpath}'")
             source_hash = hash_file(source_fpath)
             if self._source_file_index[source_fpath] != source_hash:
-              print(f"File contents changed in {source_fpath} copying it over...")
+              self._logger.info(f"File contents changed inside '{source_fpath}', copying it over to '{replica_fpath}'...")
               copy_file(source_fpath, replica_fpath)
               self._source_file_index[source_fpath] = source_hash
             continue
 
+          self._logger.debug(f"Hash cache miss for '{source_fpath}'")
           source_hash = hash_file(source_fpath)
           replica_hash = hash_file(replica_fpath)
-          print(f"Caching hash for {source_fpath}")
           self._source_file_index[source_fpath] = source_hash
           if source_hash != replica_hash:
-            print(f"File contents changed in {source_fpath} copying it over...")
+            self._logger.info(f"File contents changed inside '{source_fpath}', copying it over to '{replica_fpath}'...")
             copy_file(source_fpath, replica_fpath)
 
 
-  def _sync_replica_to_source(self):
+  def _sync_replica_to_source(self) -> None:
     """
       Walks replica directory and deletes folders and files that no longer exist in source.
     """
-    print("Deleting stale files inside of replica that no longer exist in source...")
     for dirpath, _, fnames in os.walk(self.replica_path):
       replica_rel_path = os.path.relpath(dirpath, self.replica_path)
       src_folder_path = os.path.join(self.source_path, replica_rel_path)
 
-      # delete replica folders that no longer exist in source
       if not os.path.exists(src_folder_path):
         remove_folder(dirpath)
-        print(f"Source path doesnt exist ... {src_folder_path}")
+        self._logger.info(f"Folder '{replica_rel_path}' doesn't exist in source, removing it...")
         continue
 
-      # delete files in some level of replica that no longer exist at the same level in source
       for fname in fnames:
         replica_fpath = os.path.join(dirpath, fname)
         source_fpath = os.path.join(src_folder_path, fname)
         if not os.path.exists(source_fpath):
-          print(f"Detected file that doesn't exist in source but does in replica - {replica_fpath}... removing it...")
+          self._logger.info(f"File '{replica_fpath}' in replica no longer exists in source, removing it...")
           os.remove(replica_fpath)
 
   def _setup_logger(self, log_file_path: str = None) -> None:
